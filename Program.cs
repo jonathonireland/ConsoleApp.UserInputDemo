d9a558141a95194cac3a1dbf102a6e9128f0694b
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-15 11:27:39 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Declare variables
+string name = string.Empty;
+int age = 0;
+int retirementAge = 65;
+
+Console.WriteLine("Please enter your name");
+name = Console.ReadLine();
+
+Console.WriteLine("Please enter your age");
+age = Convert.ToInt32(Console.ReadLine());
+
+// process the data
+int workingYearsRemaining = retirementAge - age;
+
+// Output the results to the user
+Console.WriteLine($"Full name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Working years remaining: {workingYearsRemaining}");
+
+
