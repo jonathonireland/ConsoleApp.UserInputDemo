26a34e378969cbe9b7a3c6abc98aa0426ceca2f7
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-17 14:13:29 -0400

    Wrapped up video 85 for Date Time Manipulation.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
-﻿// Declare variables
+﻿using System.Globalization;
+
+// Declare variables
+
+
+const int retirementAge = 65;
+
 string? firstName = string.Empty;
 string lastName = string.Empty;
 int age;
-int retirementAge = 65;
+DateOnly dob = new DateOnly();
 decimal salary = 0;
 char gender;
 bool working = false;
@@ -13,8 +19,10 @@ firstName = Console.ReadLine();
 Console.Write("Please enter your last name:");
 lastName = Console.ReadLine();
 
-Console.Write("Please enter your age:");
-age = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please enter your date of birth (dd/mm/yyyy):");
+dob = DateOnly.ParseExact(Console.ReadLine(), "dd/mm/yyyy", CultureInfo.InvariantCulture);
+
+age = DateTime.Now.Year - dob.Year;
 
 Console.Write("Please enter your salary:");
 salary = Convert.ToDecimal(Console.ReadLine());
@@ -28,12 +36,15 @@ working = Convert.ToBoolean(Console.ReadLine());
 // process the data
 int workingYearsRemaining = retirementAge - age;
 
+var estimatedRetirementYear = DateTime.Now.AddYears(workingYearsRemaining);
+
 // Output the results to the user
 Console.WriteLine($"Full name: {firstName} {lastName}");
 Console.WriteLine($"Age: {age}");
-Console.WriteLine($"Your Salary is: {salary}");
+Console.WriteLine($"Your Salary is: {salary.ToString("C")}");
 Console.WriteLine($"Your Gender is: {gender}");
 Console.WriteLine($"You Are Employed: {working}");
 Console.WriteLine($"Number of Working years remaining: {workingYearsRemaining}");
+Console.WriteLine($"Estimated Retirement year: {estimatedRetirementYear.Year}");
 
 
